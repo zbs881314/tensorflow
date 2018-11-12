cdfb5b07ef224b36cb269c9e1af1bfde6e9cc536
zbs881314 <34663445+zbs881314@users.noreply.github.com>
2018-11-12 15:54:02 -0500

    Create dropout.py

---
+++ dropout.py
@@ -0,0 +1,54 @@
+from __future__ import print_function
+import tensorflow as tf
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+
+digits = load_digits()
+X = digits.data
+y = digits.target
+y = LabelBinarizer().fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+def add_layer(inputs, in_size, out_size, layer_name, activation_function=None,):
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1,)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    # dropout
+    Wx_plus_b = tf.nn.dropout(Wx_plus_b, keep_prob)
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b,)
+    tf.summary.histogram(layer_name + '/outputs', outputs)
+    return outputs
+    
+keep_prob = tf.placeholder(tf.float32)
+xs = tf.placeholder(tf.float32, [None, 64])
+ys = tf.placeholdre(tf.float32, [None, 10])
+
+l1 = add_layer(xs, 64, 50, 'l1', activation_function=tf.nn.tanh)
+prediction = add_layer(l1, 50, 10, 'l2', activation_function=tf.nn.softmax)
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction), reduction_indices=[1]))
+tf.summary.scalar('loss', cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+sess = tf.Session()
+merged = tf.summary.merge_all()
+
+train_writer = tf.summary.FileWriter('logs/train', sess.graph)
+test_writer = tf.summary.FileWriter('logs/test', sess.graph)
+
+if int(tf.__version__).split('.')[1]) < 12 and int(tf.__version__).split('.')[0]) < 1:
+    init = tf.initialize_all_variables()
+else:
+    init = tf.global_variables_initializer()
+sess.run(init)
+for i in range(500):
+    sess.run(train_step, feed_dict={xs: X_train, ys: y_train, keep_prob: 1})
+    if i % 50 == 0:
+        train_result = sess.run(merged, feed_dict={xs: X_train, ys: y_train, keep_prob: 1})
+        test_result = sess.run(merged, feed_dict={xs: X_test, ys: y_test, keep_prob: 1})
+        train_writer.add_summary(train_result, i)
+        test_writer.add_summary(test_result, i)
