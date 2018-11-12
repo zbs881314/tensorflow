008ff1b987a837d0e9533a97a93118af98bc5357
zbs881314 <34663445+zbs881314@users.noreply.github.com>
2018-11-12 16:56:32 -0500

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -29,4 +29,44 @@ xs = tf.placeholder(tf.float32, [None, 784])/255.
 ys = tf.placeholder(tf.float32, [None, 10])
 keep_prob = tf.placeholder(tf.float32)
 x_image = tf.reshape(xs, [-1, 28, 28, 1])
+# print(x_image.shape)  [n_sample, 28, 28, 1]
 
+# conv1 layer
+W_conv1 = weight_variable([5, 5, 1, 32])   # patch 5x5, in size 1, out size 32
+b_conv1 = bias_variable([32])
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)   # output size 28x28x32
+h_pool1 = max_pool_2x2(h_conv1)   # output size 14x14x32
+
+# conv2 layer
+W_conv2 = weight_variable([5, 5, 32, 64])   # patch 5x5, in size 32, out size 64
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)   # output size 14x14x64
+h_pool2 = max_pool_2x2(h_conv2)   # output size 7x7x64
+
+# fc1 layer
+W_fc1 = weight_variable([7*7*64, 1024])
+b_fc1 = bias_variable([1024])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+# fc2 layer
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+prediction = tf.nn.softmax(te.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+# error
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction), reduction_indices=[1]))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+sess = tf.Session()
+
+if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
+    init = tf.initialize_all_variables()
+else:
+    init = tf.global_variables_initializer()
+sess.run(init)
+for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ye, keep_prob: 0.5})
+    if i % 50 == 0:
+        print(compute_accuracy(mnist.test.images[:1000], mnist.test.labels[:1000]))
