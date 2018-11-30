0c09c7c58fed63304a64729dd8c713b19db30dcc
bob <shibo.zhou14@gmail.com>
2018-11-29 21:00:49 -0500

    Update

--- sample.py
+++ sample.py
@@ -14,7 +14,7 @@ loss = tf.reduce_mean(tf.square(y - y_data))
 optimizer = tf.train.GradientDescentOptimizer(0.5)
 train = optimizer.minimize(loss)
 
-init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 
 sess = tf.Session()
 sess.run(init)
@@ -22,4 +22,4 @@ sess.run(init)
 for step in range(201):
   sess.run(train)
   if step % 20 == 0:
-    print(steps, sess.run(Weights), sess.run(biases))
+    print(step, sess.run(Weights), sess.run(biases))
